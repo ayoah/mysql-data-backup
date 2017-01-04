99013a3dfdf57da52d872172058bfafddf1a8b13
fun <ayoahy@gmail.com>
2017-01-04 15:50:36 +0800

    refs 修改打包路径

--- dbbak/bakdb.sh
+++ dbbak/bakdb.sh
@@ -29,7 +29,7 @@ function hotbackup(){
 if [ "$1" == "full" ];then
    # 全量备份
    hotbackup "full" "${fullPath}/${bakdate}.log" "none" "$fullPath/$bakdate"
-   tar zcvf "${fullPath}/${bakdate}.tar.gz" "$fullPath/$bakdate"
+   tar zcvf "${fullPath}/${bakdate}.tar.gz" -C "$fullPath" "$bakdate"
    #/usr/bin/scp -P 9922 -rp  ${fullPath}/${bakdate}* db02:${fullPath}
 
 elif [ "$1" == "incremental" ];then
@@ -37,11 +37,11 @@ elif [ "$1" == "incremental" ];then
   # 第二次开始增量备份的上一次目录指向第一次增量目录即可
   if [ "$2" == "first" ];then
      hotbackup "incremental" "${incrPath}/${bakdate}_${bakhour}.log" "$incrPath/${bakdate}_${bakhour}" "$fullPath/$bakdate"
-     tar zcvf "$incrPath/${bakdate}_${bakhour}.tar.gz" "$incrPath/${bakdate}_${bakhour}"
+     tar zcvf "$incrPath/${bakdate}_${bakhour}.tar.gz" -C "$incrPath" "${bakdate}_${bakhour}"
      #/usr/bin/scp -P 9922 -rp ${incrPath}/${bakdate}_${bakhour}* db02:${incrPath}
   else
      hotbackup "incremental" "${incrPath}/${bakdate}_${bakhour}.log" "$incrPath/${bakdate}_${bakhour}" "$incrPath/${oneHourAgo}"
-     tar zcvf "$incrPath/${bakdate}_${bakhour}.tar.gz" "$incrPath/${bakdate}_${bakhour}"
+     tar zcvf "$incrPath/${bakdate}_${bakhour}.tar.gz" -C "$incrPath" "${bakdate}_${bakhour}"
      #/usr/bin/scp -P 9922 -rp ${incrPath}/${bakdate}_${bakhour}* db02:${incrPath}
   fi
 fi
